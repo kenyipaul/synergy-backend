ca513819f3a5e40729c421729b7c5169f23d9778
Kenyi Paul <kenyipaul69@gmail.com>
2025-01-20 15:00:07 +0200

    - Switch all api handlers to sockets

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const conn = () => {
 
-    mongoose.connect(process.env.MONGO_URI);
-    // mongoose.connect(process.env.PROD_MONGO_URI);
+    // mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.PROD_MONGO_URI);
     mongoose.connection.once("open", () => console.log("Connected to MongoDB"))
     mongoose.connection.on('error', (er) => console.error(er))
 
